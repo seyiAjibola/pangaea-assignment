416652dcc9df2ab436f6266f7b02e38c2fb12a0f
seyiAjibola <so@startuppartnersafrica.com>
2021-05-06 04:15:31 +0100

    Add files via upload

---
+++ app/Http/Controllers/PangaeaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\SubscriptionService;
+use App\Http\Requests\SubscribeRequest;
+use App\Http\Requests\PublishRequest;
+
+class PangaeaController extends Controller
+{
+    protected $subscriptionService;
+
+    public function __construct(SubscriptionService $subscriptionService)
+    {
+        $this->subscriptionService = $subscriptionService;
+
+    }
+
+    public function subscribe(SubscribeRequest $request, $topic)
+    {
+        $result = $this->subscriptionService->subscribeTopic($topic, $request->url);
+        if(!$result) {
+            $response = ['status' => $result];
+            return response($response, 422);
+        }
+        $response = ['status' => true, 'data' => $result];
+        return response($response, 200);
+    }
+
+    public function publish(PublishRequest $request, $topic)
+    {
+        $result = $this->subscriptionService->publishTopic($topic, $request->url);
+        return ($result) ? response(['status' => $result, 'message' => 'Post published successfully and notification sent to subscribers'], 200) 
+                        : response(['status' => $result], 422);
+    }
+
+}
---
+++ database/seeders/SubscriptionSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Subscription;
+
+class SubscriptionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0; $i < 3; $i++) { 
+	    	Subscription::insert([
+	            'topic' => 'Test topic'.$i
+	        ]);
+    	}
+    }
+}
